195c129bb2785d2af673ccd8dd63373ac6fe26ea
Asma Khan <61154215+Asma-0101@users.noreply.github.com>
2021-12-21 13:11:03 +0530

    Add files via upload

---
+++ Migrations/20211216074552_masterTableCreation.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SBI_MF.Migrations
+{
+    public partial class masterTableCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChargeMaster",
+                columns: table => new
+                {
+                    ChargeId = table.Column<string>(type: "Varchar(10)", nullable: false),
+                    SchemeType = table.Column<string>(type: "Varchar(50)", nullable: true),
+                    TransactionType = table.Column<string>(type: "Varchar(50)", nullable: true),
+                    ChargeName = table.Column<string>(type: "Varchar(50)", nullable: true),
+                    ChargeValue = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    EffectiveDate = table.Column<DateTime>(type: "date", nullable: false),
+                    Remarks = table.Column<string>(type: "Varchar(250)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChargeMaster", x => x.ChargeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CounterPartyMaster",
+                columns: table => new
+                {
+                    CounterPartyID = table.Column<string>(type: "varchar(15)", nullable: false),
+                    CounterpartyName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    PAN = table.Column<string>(type: "varchar(10)", nullable: true),
+                    GSTNo = table.Column<string>(type: "varchar(15)", nullable: true),
+                    Address1 = table.Column<string>(type: "varchar(250)", nullable: true),
+                    Address2 = table.Column<string>(type: "varchar(250)", nullable: true),
+                    Address3 = table.Column<string>(type: "varchar(250)", nullable: true),
+                    EmailId1 = table.Column<string>(type: "varchar(50)", nullable: true),
+                    EmailId2 = table.Column<string>(type: "varchar(50)", nullable: true),
+                    TelNo1 = table.Column<string>(type: "varchar(50)", nullable: true),
+                    TelNo2 = table.Column<string>(type: "varchar(50)", nullable: true),
+                    FaxNo1 = table.Column<string>(type: "varchar(50)", nullable: true),
+                    FaxNo2 = table.Column<string>(type: "varchar(50)", nullable: true),
+                    MobNo1 = table.Column<string>(type: "varchar(50)", nullable: true),
+                    MobNo2 = table.Column<string>(type: "varchar(50)", nullable: true),
+                    ContactPerson = table.Column<string>(type: "varchar(50)", nullable: true),
+                    BankName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    BankBranch = table.Column<string>(type: "varchar(50)", nullable: true),
+                    IFSC = table.Column<string>(type: "varchar(50)", nullable: true),
+                    NameBeneficiary = table.Column<string>(type: "varchar(50)", nullable: true),
+                    AccountNo = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CounterPartyMaster", x => x.CounterPartyID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustodianMaster",
+                columns: table => new
+                {
+                    CustodianId = table.Column<string>(type: "varchar(15)", nullable: false),
+                    CustodianName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Address1 = table.Column<string>(type: "varchar(250)", nullable: true),
+                    Address2 = table.Column<string>(type: "varchar(250)", nullable: true),
+                    Address3 = table.Column<string>(type: "varchar(250)", nullable: true),
+                    EmailId1 = table.Column<string>(type: "varchar(50)", nullable: true),
+                    EmailId2 = table.Column<string>(type: "varchar(50)", nullable: true),
+                    TelephoneNumber1 = table.Column<string>(type: "varchar(15)", nullable: true),
+                    TelephoneNumber2 = table.Column<string>(type: "varchar(15)", nullable: true),
+                    FaxNumber1 = table.Column<string>(type: "varchar(10)", nullable: true),
+                    FaxNumber2 = table.Column<string>(type: "varchar(10)", nullable: true),
+                    MobileNumber1 = table.Column<string>(type: "varchar(15)", nullable: true),
+                    MobileNumber2 = table.Column<string>(type: "varchar(15)", nullable: true),
+                    ContactPerson = table.Column<string>(type: "varchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustodianMaster", x => x.CustodianId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GoldLocationMaster",
+                columns: table => new
+                {
+                    Lid = table.Column<string>(type: "varchar(15)", nullable: false),
+                    GoldLocation = table.Column<string>(nullable: true),
+                    Active = table.Column<string>(type: "char(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GoldLocationMaster", x => x.Lid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GoldMaster",
+                columns: table => new
+                {
+                    GoldId = table.Column<string>(type: "varchar(15)", nullable: false),
+                    SecurityName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Active = table.Column<string>(type: "char(1)", nullable: true),
+                    SecurityLocation = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Type = table.Column<string>(type: "varchar(50)", nullable: true),
+                    BarWeightInKg = table.Column<float>(nullable: false),
+                    BarWeightInGrams = table.Column<float>(nullable: false),
+                    CommodityPurity = table.Column<decimal>(type: "numeric(20,4)", nullable: false),
+                    CommodityDenomination = table.Column<string>(type: "varchar(10)", nullable: true),
+                    ValuationRateType = table.Column<string>(type: "varchar(10)", nullable: true),
+                    Conversionfactor = table.Column<decimal>(type: "numeric(20,4)", nullable: false),
+                    VaultingAgent = table.Column<string>(type: "varchar(50)", nullable: true),
+                    CounterpartyShipper = table.Column<string>(type: "varchar(150)", nullable: true),
+                    Currency = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Issuer = table.Column<string>(type: "varchar(50)", nullable: true),
+                    StockExchange = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Stampduty = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    Octroi = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    Premium = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    CustomDuty = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    FixingCharge = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    SGST = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    CGST = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    GST = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    NAVlot = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    Facevalue = table.Column<decimal>(type: "numeric(3,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GoldMaster", x => x.GoldId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaxMaster",
+                columns: table => new
+                {
+                    TaxId = table.Column<string>(type: "varchar(15)", nullable: false),
+                    State = table.Column<string>(nullable: true),
+                    SGST = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    CGST = table.Column<decimal>(type: "numeric(3,2)", nullable: false),
+                    GST = table.Column<decimal>(type: "numeric(3,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaxMaster", x => x.TaxId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserMaster",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Password = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Roles = table.Column<string>(type: "varchar(50)", nullable: true),
+                    UserGroup = table.Column<string>(type: "varchar(50)", nullable: true),
+                    ModuleRights = table.Column<string>(type: "varchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMaster", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChargeMaster");
+
+            migrationBuilder.DropTable(
+                name: "CounterPartyMaster");
+
+            migrationBuilder.DropTable(
+                name: "CustodianMaster");
+
+            migrationBuilder.DropTable(
+                name: "GoldLocationMaster");
+
+            migrationBuilder.DropTable(
+                name: "GoldMaster");
+
+            migrationBuilder.DropTable(
+                name: "TaxMaster");
+
+            migrationBuilder.DropTable(
+                name: "UserMaster");
+        }
+    }
+}
---
+++ Migrations/20211220114423_workflowTableCreation.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SBI_MF.Migrations
+{
+    public partial class workflowTableCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustodianInstruction",
+                columns: table => new
+                {
+                    CustodianInstructionId = table.Column<string>(type: "varchar(15)", nullable: false),
+                    TransactionId = table.Column<string>(type: "varchar(10)", nullable: true),
+                    CustodianName = table.Column<string>(type: "varchar(20)", nullable: true),
+                    Address = table.Column<string>(type: "varchar(250)", nullable: true),
+                    ContactNo = table.Column<string>(type: "varchar(15)", nullable: true),
+                    ContactPerson = table.Column<string>(type: "varchar(50)", nullable: true),
+                    SecurityName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    TradeDate = table.Column<DateTime>(type: "date", nullable: false),
+                    SettlementDate = table.Column<DateTime>(type: "date", nullable: false),
+                    WeightOfGoldBar = table.Column<string>(type: "varchar(50)", nullable: true),
+                    PurityOfGold = table.Column<string>(type: "varchar(20)", nullable: true),
+                    Location = table.Column<string>(type: "varchar(50)", nullable: true),
+                    VaultLocation = table.Column<string>(type: "varchar(50)", nullable: true),
+                    CounterParty = table.Column<string>(type: "varchar(20)", nullable: true),
+                    DelRefNo = table.Column<string>(type: "varchar(20)", nullable: true),
+                    QtyOfGoldBar = table.Column<decimal>(type: "numeric(20,2)", nullable: false),
+                    Total = table.Column<decimal>(type: "numeric(20,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustodianInstruction", x => x.CustodianInstructionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DealConfirmation",
+                columns: table => new
+                {
+                    DealConfirmId = table.Column<string>(type: "varchar(15)", nullable: false),
+                    TransactionId = table.Column<string>(type: "varchar(15)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Address = table.Column<string>(type: "varchar(250)", nullable: false),
+                    ContactNo = table.Column<string>(type: "varchar(25)", nullable: false),
+                    ContactPersonName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    DealRefNo = table.Column<string>(type: "varchar(20)", nullable: false),
+                    ClientName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    SchemeName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    TransactionType = table.Column<string>(type: "varchar(50)", nullable: false),
+                    GSTNo = table.Column<string>(type: "varchar(50)", nullable: false),
+                    DealDate = table.Column<DateTime>(type: "date", nullable: false),
+                    ValueDate = table.Column<DateTime>(type: "date", nullable: false),
+                    Commodity = table.Column<string>(type: "varchar(150)", nullable: false),
+                    CounterpartyShipper = table.Column<string>(type: "varchar(150)", nullable: false),
+                    QuantityInKilogram = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    Rate = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    SGST = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    CGST = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    GST = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    TotalGST = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    TotalConsideration = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    OtherApplicableTaxes = table.Column<string>(type: "varchar(150)", nullable: true),
+                    TaxCollectedAtSource = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    NetConsideration = table.Column<string>(type: "varchar(250)", nullable: false),
+                    Remarks = table.Column<string>(type: "varchar(250)", nullable: true),
+                    BranchName = table.Column<string>(type: "varchar(250)", nullable: true),
+                    IFSCCode = table.Column<string>(type: "varchar(50)", nullable: true),
+                    BeneficiaryAccount = table.Column<decimal>(type: "numeric(18,0)", nullable: false),
+                    BeneficiaryAccountName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    DealStatus = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DealConfirmation", x => x.DealConfirmId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionCapture",
+                columns: table => new
+                {
+                    TransactionId = table.Column<string>(type: "varchar(15)", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "date", nullable: false),
+                    SettlementTenor = table.Column<string>(type: "varchar(10)", nullable: false),
+                    ValueDate = table.Column<DateTime>(type: "date", nullable: false),
+                    Counterparty = table.Column<string>(type: "varchar(50)", nullable: false),
+                    SchemeName = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Security = table.Column<string>(type: "varchar(50)", nullable: false),
+                    SecurityLocation = table.Column<string>(type: "varchar(50)", nullable: false),
+                    DealValue = table.Column<decimal>(type: "numeric(20,2)", nullable: false),
+                    QuantityInKg = table.Column<decimal>(type: "numeric(20,2)", nullable: false),
+                    NoOfUnitsPerKg = table.Column<decimal>(type: "numeric(20,2)", nullable: false),
+                    TotalUnits = table.Column<decimal>(type: "numeric(20,2)", nullable: false),
+                    TransactionStatus = table.Column<string>(type: "char(1)", nullable: true),
+                    TransactionType = table.Column<string>(type: "char(1)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionCapture", x => x.TransactionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Valuation",
+                columns: table => new
+                {
+                    ValuationId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TransactionId = table.Column<int>(nullable: false),
+                    Workflow = table.Column<string>(type: "varchar(50)", nullable: true),
+                    TransactionType = table.Column<string>(type: "char(1)", nullable: true),
+                    LondonAMRateUSD = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    FixingChargesUSD = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    PremiumUSD = table.Column<float>(type: "real", nullable: false),
+                    MetalRateUSD = table.Column<float>(type: "real", nullable: false),
+                    ConversionFactor = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    RBIReferenceRateINR = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    MetalRatePerkgINR = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    CustomsDutyKg = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    StampDutyINR = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    FinalPriceUSD = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    TransactionStatus = table.Column<string>(type: "char(1)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Valuation", x => x.ValuationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustodianInstruction");
+
+            migrationBuilder.DropTable(
+                name: "DealConfirmation");
+
+            migrationBuilder.DropTable(
+                name: "TransactionCapture");
+
+            migrationBuilder.DropTable(
+                name: "Valuation");
+        }
+    }
+}
