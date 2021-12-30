1f1501a56f6d521bda330dc95d3ee2e0961d13e5
Asma Khan <61154215+Asma-0101@users.noreply.github.com>
2021-12-30 12:14:12 +0530

    added get and authorize put

--- Controllers/DealConfirmationController.cs
+++ Controllers/DealConfirmationController.cs
@@ -29,51 +29,154 @@ namespace SBI_MF.Controllers
             return await _context.DealConfirmation.ToListAsync();
         }
 
-        // GET: api/DealConfirmation/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<DealConfirmationModel>> GetDealConfirmationModel(string id)
+        
+        // GET: api/DealConfirmation
+        [HttpGet("GetSales")]
+        public IQueryable<DealConfirmationDto> GetSales()
         {
-            var dealConfirmationModel = await _context.DealConfirmation.FindAsync(id);
-
-            if (dealConfirmationModel == null)
-            {
-                return NotFound();
-            }
-
-            return dealConfirmationModel;
-        }
-
-        // PUT: api/DealConfirmation/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutDealConfirmationModel(string id, DealConfirmationModel dealConfirmationModel)
-        {
-            if (id != dealConfirmationModel.DealConfirmId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(dealConfirmationModel).State = EntityState.Modified;
-
+            // return await _context.DealConfirmation.ToListAsync();
             try
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!DealConfirmationModelExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+                var transaction = from t in _context.DealConfirmation.Where(b => b.DealStatus == "Incomplete" && b.TransactionType == "S")
+                                  select new DealConfirmationDto()
+                                  {
+                                      TransactionId = t.TransactionId,
+                                      DealConfirmId = t.DealConfirmId,
+                                      Name = t.Name,
+                                      Address = t.Address,
+                                      ContactNo = t.ContactNo,
+                                      ContactPersonName = t.ContactPersonName,
+                                      DealRefNo = t.DealRefNo,
+                                      ClientName = t.ClientName,
+                                      SchemeName = t.SchemeName,
+                                      TransactionType = t.TransactionType,
+                                      GSTNo = t.GSTNo,
+                                      DealDate = t.DealDate,
+                                      ValueDate = t.ValueDate,
+                                      Commodity = t.Commodity,
+                                      CounterpartyShipper = t.CounterpartyShipper,
+                                      QuantityInKilogram = t.QuantityInKilogram,
+                                      Rate = t.Rate,
+                                      TotalPrice = t.TotalPrice,
+                                      SGST = t.SGST,
+                                      CGST = t.CGST,
+                                      GST = t.GST,
+                                      TotalGST = t.TotalGST,
+                                      TotalConsideration = t.TotalConsideration,
+                                      OtherApplicableTaxes = t.OtherApplicableTaxes,
+                                      TaxCollectedAtSource = t.TaxCollectedAtSource,
+                                      NetConsideration = t.NetConsideration,
+                                      Remarks = t.Remarks,
+                                      BranchName = t.BranchName,
+                                      IFSCCode = t.IFSCCode,
+                                      BeneficiaryAccount = t.BeneficiaryAccount,
+                                      BeneficiaryAccountName = t.BeneficiaryAccountName,
+                                      DealStatus = t.DealStatus
+                                  };
 
-            return NoContent();
+                return transaction;
+
+            }
+            catch
+            {
+                throw;
+            }
         }
+
+        [HttpGet("GetPurchase")]
+        public IQueryable<DealConfirmationDto> GetPurchase()
+        {
+            // return await _context.DealConfirmation.ToListAsync();
+            try
+            {
+                var transaction = from t in _context.DealConfirmation.Where(b => b.DealStatus == "Incomplete" && b.TransactionType == "P")
+                                  select new DealConfirmationDto()
+                                  {
+                                      TransactionId = t.TransactionId,
+                                      DealConfirmId = t.DealConfirmId,
+                                      Name = t.Name,
+                                      Address = t.Address,
+                                      ContactNo = t.ContactNo,
+                                      ContactPersonName = t.ContactPersonName,
+                                      DealRefNo = t.DealRefNo,
+                                      ClientName = t.ClientName,
+                                      SchemeName = t.SchemeName,
+                                      TransactionType = t.TransactionType,
+                                      GSTNo = t.GSTNo,
+                                      DealDate = t.DealDate,
+                                      ValueDate = t.ValueDate,
+                                      Commodity = t.Commodity,
+                                      CounterpartyShipper = t.CounterpartyShipper,
+                                      QuantityInKilogram = t.QuantityInKilogram,
+                                      Rate = t.Rate,
+                                      TotalPrice = t.TotalPrice,
+                                      SGST = t.SGST,
+                                      CGST = t.CGST,
+                                      GST = t.GST,
+                                      TotalGST = t.TotalGST,
+                                      TotalConsideration = t.TotalConsideration,
+                                      OtherApplicableTaxes = t.OtherApplicableTaxes,
+                                      TaxCollectedAtSource = t.TaxCollectedAtSource,
+                                      NetConsideration = t.NetConsideration,
+                                      Remarks = t.Remarks,
+                                      DealStatus = t.DealStatus
+                                  };
+
+                return transaction;
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
+        [HttpGet("id")]
+
+        public IQueryable<DealConfirmationDto> GetbyDRN(string drn)
+        {
+            var transaction = from t in _context.DealConfirmation.Where(b => b.DealRefNo == drn && b.TransactionType == "S" && b.DealStatus == "Complete")
+                              select new DealConfirmationDto()
+                              {
+                                  TransactionId = t.TransactionId,
+                                  DealConfirmId = t.DealConfirmId,
+                                  Name = t.Name,
+                                  Address = t.Address,
+                                  ContactNo = t.ContactNo,
+                                  ContactPersonName = t.ContactPersonName,
+                                  DealRefNo = t.DealRefNo,
+                                  ClientName = t.ClientName,
+                                  SchemeName = t.SchemeName,
+                                  TransactionType = t.TransactionType,
+                                  GSTNo = t.GSTNo,
+                                  DealDate = t.DealDate,
+                                  ValueDate = t.ValueDate,
+                                  Commodity = t.Commodity,
+                                  CounterpartyShipper = t.CounterpartyShipper,
+                                  QuantityInKilogram = t.QuantityInKilogram,
+                                  Rate = t.Rate,
+                                  TotalPrice = t.TotalPrice,
+                                  SGST = t.SGST,
+                                  CGST = t.CGST,
+                                  GST = t.GST,
+                                  TotalGST = t.TotalGST,
+                                  TotalConsideration = t.TotalConsideration,
+                                  OtherApplicableTaxes = t.OtherApplicableTaxes,
+                                  TaxCollectedAtSource = t.TaxCollectedAtSource,
+                                  NetConsideration = t.NetConsideration,
+                                  Remarks = t.Remarks,
+                                  BranchName = t.BranchName,
+                                  IFSCCode = t.IFSCCode,
+                                  BeneficiaryAccount = t.BeneficiaryAccount,
+                                  BeneficiaryAccountName = t.BeneficiaryAccountName,
+                                  DealStatus = t.DealStatus
+
+                              };
+
+            return transaction;
+        }
+
 
         // POST: api/DealConfirmation
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
@@ -100,6 +203,59 @@ namespace SBI_MF.Controllers
 
             return CreatedAtAction("GetDealConfirmationModel", new { id = dealConfirmationModel.DealConfirmId }, dealConfirmationModel);
         }
+        
+        [HttpPut("{id}")]
+        
+        public async Task<IActionResult> ConfirmDeal(string id, DealConfirmationModel DealConfirmationModel, string Task)
+        {
+            if (id != DealConfirmationModel.TransactionId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(DealConfirmationModel).State = EntityState.Modified;
+
+            try
+            {
+                if (Task == "Authorize")
+                {
+                    if (DealConfirmationModel.DealStatus == "Incomplete")
+                    {
+                        var dto = new DealConfirmationDto()
+                        {
+                            DealStatus = DealConfirmationModel.DealStatus = "Complete",
+
+                        };
+
+                        await _context.SaveChangesAsync();
+                    
+                        CustodianInstructionModel custodianInstructionModel1 = new CustodianInstructionModel();
+  
+                        var data = new DealConfirmationDto()
+                        {
+                            DealRefNo = DealConfirmationModel.DealRefNo = custodianInstructionModel1.DelRefNo 
+      
+                        };
+                    }
+                }
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DealConfirmationModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
 
         // DELETE: api/DealConfirmation/5
         [HttpDelete("{id}")]
