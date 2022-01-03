e34b39f8b67328bfb8c65b39fda4e892196ad1cf
Asma Khan <61154215+Asma-0101@users.noreply.github.com>
2022-01-03 16:41:16 +0530

    Update ValuationController.cs

--- Controllers/ValuationController.cs
+++ Controllers/ValuationController.cs
@@ -78,27 +78,96 @@ namespace SBI_MF.Controllers
         // POST: api/Valuation
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<ValuationModel>> PostValuationModel(ValuationModel valuationModel)
+          [HttpPost("test")]
+        public async Task<List<ValuationModel>> uploadExcel(IFormFile file1)
         {
-            _context.Valuation.Add(valuationModel);
             try
+            { 
+            var chargeData=new List<ValuationModel>();
+            var fileList=HttpContext.Request.Form.Files;
+            DataSet dsexcelRecords = new DataSet();
+            IExcelDataReader reader = null;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            if(fileList!=null)
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
+            using(var fs=new MemoryStream())
             {
-                if (ValuationModelExists(valuationModel.ValuationId))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
+               foreach(var file in fileList)
+            {
+                await file.CopyToAsync(fs);
             }
+                
+                using (reader = ExcelReaderFactory.CreateReader(fs))
+                {
+                    dsexcelRecords = reader.AsDataSet();
+                    //if(reader.RowCount>1)
+                     if (dsexcelRecords != null && dsexcelRecords.Tables.Count > 0)
+                    {            
+                        DataTable dtStudentRecords = dsexcelRecords.Tables[0];
+                                //while (reader.Read()) 
+                                using (SqlConnection con = new SqlConnection("server=NUCDBSRV;database=SBI_MF;user id=sa;password=nuc1234$;"))
 
-            return CreatedAtAction("GetValuationModel", new { id = valuationModel.ValuationId }, valuationModel);
+                                {
+                                    for (int i = 1; i < dtStudentRecords.Rows.Count; i++)
+
+                                    {
+                                        SqlCommand cmd = new SqlCommand("ValuationInsert", con);
+                                        cmd.CommandType = CommandType.StoredProcedure;                                
+                                        cmd.Parameters.AddWithValue("@ValuationId",dtStudentRecords.Rows[i][0]);
+                                        cmd.Parameters.AddWithValue("@TransactionId",dtStudentRecords.Rows[i][1]);
+                                        cmd.Parameters.AddWithValue("@Workflow",dtStudentRecords.Rows[i][2]);
+                                        cmd.Parameters.AddWithValue("@TransactionType",reader.GetValue(3).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@LondonAMRateUSD",reader.GetValue(4).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@FixingChargesUSD",reader.GetValue(5).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@PremiumUSD",reader.GetValue(6).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@MetalRateUSD",reader.GetValue(7).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@ConversionFactor",reader.GetValue(8).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@RBIReferenceRateINR",reader.GetValue(9).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@MetalRatePerkgINR",reader.GetValue(10).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@CustomsDutyKg",reader.GetValue(11).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@StampDutyINR",reader.GetValue(12).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@FinalPriceUSD",reader.GetValue(13).ToString().Trim());
+                                        cmd.Parameters.AddWithValue("@TransactionStatus","N");
+                                        
+                                        con.Open();                                
+                                        cmd.ExecuteNonQuery(); 
+                                        con.Close();
+                                    } 
+                                        
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Empty File!");
+                    }
+                }
+                // using(var xl=new ExcelPackage(fs))
+                // //using(var xl=new ExcelDataReader())
+                // {
+                //     ExcelWorksheet sheet= xl.Workbook.Worksheets[0];
+                //     var rows=sheet.Dimension.Rows;
+                //     for (int r = 2; r < rows; r++)
+                //     {
+                //         chargeData.Add(new ChargeMaster
+                //         { 
+                //             SchemeType=sheet.Cells[r,1].Value.ToString().Trim(),
+                //             TransactionType=sheet.Cells[r,1].Value.ToString().Trim(),
+                //             ChargeName=sheet.Cells[r,1].Value.ToString().Trim(),
+                //             ChargeValue=sheet.Cells[r,1].Value.ToString().Trim(),
+                //             EffectiveDate=sheet.Cells[r,1].Value.ToString().Trim(),
+                //             Remarks=sheet.Cells[r,1].Value.ToString().Trim()
+                //         });
+                //     }
+                // }
+            } 
+            }           
+            return chargeData;
+            }
+            catch(Exception)
+            {
+                throw;
+            }          
+            
         }
 
         // DELETE: api/Valuation/5
